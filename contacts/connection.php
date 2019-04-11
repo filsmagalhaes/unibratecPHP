12830afe40db9abc77be388cd1231d8febbb5d05
filsmagalhaes <43623523+filsmagalhaes@users.noreply.github.com>
2019-04-10 21:12:32 -0300

    add query

--- contacts/connection.php
+++ contacts/connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-$connection = new PDO('mysql:host=localhost;dbname=contacts', 'root', '');
+$connection = new PDO('mysql@localhost:3306;dbname=contacts', 'root', '#@IBratec##123@Aluno');
 
 //statement (instrução para inserção no banco)
 $stmt = $connection->prepare('INSERT INTO people (nome, phone, email) VALUES ("Bill Gates", "81-55887456", "billlindo@msn.com")'); 
---
+++ contacts/query/contacts.sql
@@ -0,0 +1,11 @@
+create database contacts;
+
+use contacts;
+
+create table people (
+nome varchar (200) PRIMARY KEY, 
+phone varchar (50),
+email varchar (200) 
+);
+
+select * from people;
