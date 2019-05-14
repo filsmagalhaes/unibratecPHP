f51013ada4938e04445e35f00d3db1d8121df69d
filsmagalhaes <43623523+filsmagalhaes@users.noreply.github.com>
2019-05-14 20:05:20 -0300

    CRIAÇÃO DO PROJETO

---
+++ projetoApp/Php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+CADASTRO
+<nav>
+TESTE
+</nav>
+</header>
+<div>
+    <form>
+        <label> Nome Fantasia </label><br>
+        <input type="text" name="fantasia" size="20" maxlength="100"><br>
+        <label> CNPJ </label><br>
+        <input type="text" name="cnpj" size="20" maxlength="100"><br>
+        <label> Telefone </label><br>
+        <input type="text" name="fone" size="20" maxlength="100"><br>
+        <label> Email </label><br>
+        <input type="text" name="mail" size="20" maxlength="100"><br>
+        <br>
+        <button type="button">ENVIAR</button>
+    </form>
+    </div>
+</body>
+</html>
