37c3283dbda7b09b72e16e3ea4986fa3eaba46b5
filsmagalhaes <43623523+filsmagalhaes@users.noreply.github.com>
2019-05-15 20:39:59 -0300

    Delete connection.php

--- contacts/connection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$connection = new PDO('mysql@localhost:3306;dbname=contacts', 'root', '#@IBratec##123@Aluno');
-
-//statement (instrução para inserção no banco)
-$stmt = $connection->prepare('INSERT INTO people (nome, phone, email) VALUES ("Bill Gates", "81-55887456", "billlindo@msn.com")'); 
-
-$stmt->execute();
